23db39987032c8d3c5ff64cd85592d5b66c64c23
Victor Miroshnikov <me@vmiroshnikov.com>
2015-06-26 15:25:02 +0200

    Update Kruskal.hs

--- src/Kruskal.hs
+++ src/Kruskal.hs
@@ -29,7 +29,7 @@ kruskal weight graph = runEquivM' $ filterM go sorted
       go (u,v) = do eq <- equivalent u v           -- we check whether we have an intersection in our sets
                     traceM $ "equating " ++ show (u, v, eq)
                     if eq then return False        -- we have it in our vert set
-                    else equate u v >> return True -- we don't have it
+                    else equate u v >> return True -- we grow our union and tell filter to keep this node
 
 fromL :: forall c a. Ord a => [(a, c)] -> a -> c
 fromL xs = fromJust . flip Map.lookup (Map.fromList xs)
@@ -37,6 +37,17 @@ fromL xs = fromJust . flip Map.lookup (Map.fromList xs)
 main :: IO () 
 main = mapM_ (putStrLn . show) $ kruskal w g
   where
+    {- 
+    example graphviz repr:
+        graph G {
+                1 -- 2 [label="w=1", weight=1]
+                2 -- 3 [label="w=4", weight=4]
+                3 -- 4 [label="w=5", weight=5]
+                1 -- 4 [label="w=30", weight=30]
+                1 -- 3 [label="w=4", weight=4]
+        }
+        
+    -}
     -- edges + weights
     w = fromL [ ((1,2),1)
               , ((2,3),4)
